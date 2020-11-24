951d2b3a96ab2c443a766e086ef73e286c529a3e
Andrew <vozniukandrey@gmail.com>
2020-11-24 19:56:17 +0200

    Updates

---
+++ precache-manifest.63e37e54b7ba02ff1b38b98ff49a563d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55dd9590f7c5e36947529c0aa528f6b",
+    "url": "/github-profile-finder/index.html"
+  },
+  {
+    "revision": "6f0582abfb5f29b9eef9",
+    "url": "/github-profile-finder/static/css/main.23b561a0.chunk.css"
+  },
+  {
+    "revision": "727c11df4a401f6275cb",
+    "url": "/github-profile-finder/static/js/2.29743668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profile-finder/static/js/2.29743668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0582abfb5f29b9eef9",
+    "url": "/github-profile-finder/static/js/main.3ad67f56.chunk.js"
+  },
+  {
+    "revision": "27649a45b2d007382c84",
+    "url": "/github-profile-finder/static/js/runtime-main.bc9c730c.js"
+  }
+]);
